18611550746a67961247e3ca5eaac362be9fb259
grisha-topolev <gorbacvlad81@gmail.com>
2025-04-17 12:54:13 +0300

    new feature added

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/city')
+def show_city():
+    return 'Minsk'
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
